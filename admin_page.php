10a629adc5be3793c2a478ce5b4eae19ece6c4d1
felixjorge <45113522+felixjorge@users.noreply.github.com>
2019-01-22 03:21:01 -0500

    Update admin_page.php

--- admin_page.php
+++ admin_page.php
@@ -8,7 +8,7 @@ if(!($login -> sessionID)){
 $mem = new member();
 $mem -> finduserbyID($login -> sessionID);
 ?>
-
+<!-- VIEW --> 
 <html>
 	<head>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
